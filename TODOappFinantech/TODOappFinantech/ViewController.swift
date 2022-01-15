c396e161b05bee061ce4f7d0273413db72679abf
mikok42 <mikolaj@linczewski.com>
2022-01-15 11:18:44 +0100

    initial commit

---
+++ TODOappFinantech/TODOappFinantech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TODOappFinantech
+//
+//  Created by Mikołaj Linczewski on 15/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
